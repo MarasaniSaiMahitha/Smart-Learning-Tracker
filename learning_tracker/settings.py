6c7356a10c7b19bbd3826f15380e2d7083fc3fea
MarasaniSaiMahitha <mahithamarasani@gmail.com>
2026-08-25 17:54:24 +0530

    Configure allowed hosts for Render

--- learning_tracker/settings.py
+++ learning_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k4x!_(ph8mn+&_26frrvr1x-k8hifh-z*pe(g#zm&tb!(d(&(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
